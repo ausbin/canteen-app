31096dc99885c72feaff2cf9068ac4d62a23a0cd
Austin Adams <git@austinjadams.com>
2016-10-29 18:37:23 -0400

    Show weirdness of gnu-team/canteen-app#30

--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -116,8 +116,8 @@ public class MainController implements MainAppReceiver {
      * Shows the report list screen.
      */
     public void showReportList() {
-        showView("ReportList");
         closeDrawer();
+        showView("ReportList");
     }
 
     /**
